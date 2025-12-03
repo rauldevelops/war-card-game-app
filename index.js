48c2e500072c2df144c2f8fdd4bdcdbc944be7e0
Raul Develops <124627746+rauldevelops@users.noreply.github.com>
2025-12-03 17:24:30 -0500

    added final score and game reset features

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
 let deckId
+let computerScore = 0
+let myScore = 0
 const cardsContainer = document.getElementById("cards")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
 const header = document.getElementById("header")
-const remainingCards = document.getElementById("remaining-cards")
+const remainingText = document.getElementById("remaining")
+const computerScoreEl = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
+
+drawCardBtn.disabled = true
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             deckId = data.deck_id
-            remainingCards.textContent = `Cards remaining: ${data.remaining}`
+            drawCardBtn.disabled = false
+            computerScore = 0
+            myScore = 0
+            computerScoreEl.textContent = `Computer score: ${computerScore}`
+            myScoreEl.textContent = `My score: ${myScore}`
+            header.textContent = `New Game of War`
+            cardsContainer.children[0].innerHTML = ``
+            cardsContainer.children[1].innerHTML = ``
         })
 }
 
 newDeckBtn.addEventListener("click", handleClick)
 
-/**
- * Challenge:
- * 
- * Display the number of cards remaining in the deck on the page
- * Hint: Check the data that comes back when we draw 2 new cards
- * to see if there's anything helpful there for this task (😉)
- */
-
 drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
             cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
             `
@@ -36,9 +43,27 @@ drawCardBtn.addEventListener("click", () => {
             `
             const winnerText = determineCardWinner(data.cards[0], data.cards[1])
             header.textContent = winnerText
-            remainingCards.textContent = `Cards remaining: ${data.remaining}`
+            
+            if (data.remaining === 0) {
+                drawCardBtn.disabled = true
+                if (computerScore > myScore) {
+                    header.textContent = `YOU LOST THE WAR!`
+                } else if (computerScore < myScore) {
+                    header.textContent = `YOU WON THE WAR!`
+                } else {
+                    header.textContent = `THE WAR ENDS IN A STALEMATE!`
+                }
+            }
+            
         })
 })
+
+/**
+ * Challenge:
+ * 
+ * Display the final winner in the header at the top by
+ * replacing the text of the h2.
+ */
 
 function determineCardWinner(card1, card2) {
     const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
@@ -47,9 +72,13 @@ function determineCardWinner(card1, card2) {
     const card2ValueIndex = valueOptions.indexOf(card2.value)
     
     if (card1ValueIndex > card2ValueIndex) {
-        return "Card 1 wins!"
+        computerScore++
+        computerScoreEl.textContent = `Computer score: ${computerScore}`
+        return "Computer wins!"
     } else if (card1ValueIndex < card2ValueIndex) {
-        return "Card 2 wins!"
+        myScore++
+        myScoreEl.textContent = `My score: ${myScore}`
+        return "You win!"
     } else {
         return "War!"
     }
