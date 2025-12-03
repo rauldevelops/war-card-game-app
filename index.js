ae0b8541e7b1a6ce094cc3cce087ee89435032e8
Raul Develops <124627746+rauldevelops@users.noreply.github.com>
2025-12-03 16:07:29 -0500

    Added card count feature

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 let deckId
 const cardsContainer = document.getElementById("cards")
-const winnerIs = document.getElementById("winner-is")
 const newDeckBtn = document.getElementById("new-deck")
 const drawCardBtn = document.getElementById("draw-cards")
+const header = document.getElementById("header")
+const remainingCards = document.getElementById("remaining-cards")
 
 function handleClick() {
     fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
         .then(res => res.json())
         .then(data => {
             deckId = data.deck_id
+            remainingCards.textContent = `Cards remaining: ${data.remaining}`
         })
 }
 
 newDeckBtn.addEventListener("click", handleClick)
+
+/**
+ * Challenge:
+ * 
+ * Display the number of cards remaining in the deck on the page
+ * Hint: Check the data that comes back when we draw 2 new cards
+ * to see if there's anything helpful there for this task (😉)
+ */
 
 drawCardBtn.addEventListener("click", () => {
     fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
@@ -24,49 +34,24 @@ drawCardBtn.addEventListener("click", () => {
             cardsContainer.children[1].innerHTML = `
                 <img src=${data.cards[1].image} class="card" />
             `
-            winnerIs.innerHTML = determineCardWinner(data.cards[0], data.cards[1])
-            
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            header.textContent = winnerText
+            remainingCards.textContent = `Cards remaining: ${data.remaining}`
         })
 })
-/**
- * Challenge:
- * 
- * Try to determine which of the 2 cards is the "winner" (has higher value)
- * Aces are the card with the highest "score"
- * 
- * Part 2:
- * Instead of logging the winner to the console, 
- * display an `h2` on the screen above the 2 cards 
- * that declares who the winner is.
- * 
- * If card1 is the higher card, display "Computer wins!"
- * If card2 is the higher card, display "You win!"
- * If they're equal, display "War!"
- */
 
 function determineCardWinner(card1, card2) {
     const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
     "10", "JACK", "QUEEN", "KING", "ACE"]
     const card1ValueIndex = valueOptions.indexOf(card1.value)
     const card2ValueIndex = valueOptions.indexOf(card2.value)
-    console.log("card 1:", card1ValueIndex)
-    console.log("card 2:", card2ValueIndex)
     
     if (card1ValueIndex > card2ValueIndex) {
-        return "Computer wins!"
+        return "Card 1 wins!"
     } else if (card1ValueIndex < card2ValueIndex) {
-        return "You win!"
+        return "Card 2 wins!"
     } else {
         return "War!"
     }
 }
 
-// Keeping this test case for reference, but should delete later
-// const card1Obj = {
-//     value: "JACK"
-// }
-// const card2Obj = {
-//     value: "QUEEN"
-// }
-
-// determineCardWinner(card1Obj, card2Obj)
